7528728679f64e13a02edeb1b834c9e5218b53f4
arifeng <ari.feng@forensix.cn>
2014-12-01 23:41:33 +0800

    remove old tests

--- test/gcc/test.c
+++
@@ -1,8 +0,0 @@
-﻿#include "../test-c.h"
-
-int main(int argc, char*argv[])
-{
-	do_test_c(argc, argv);
-
-	return 0;
-}
--- test/gcc/test.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include "../test-cpp.h"
-
-int main(int argc, char*argv[])
-{
-	do_test_cpp(argc, argv);
-
-	return 0;
-}
